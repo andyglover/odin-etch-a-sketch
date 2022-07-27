79e7d3f9f867e3096618211e5a0df970e70f7d7f
Andy Glover <andrew.l.glover@gmail.com>
2022-07-26 22:17:13 -0700

    Create grid of squares with css and js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+
+
+function createGridSquares(){
+    createLineBreakDiv();
+    for(let i=1; i<=16; i++){
+        console.log("Hello world");
+        const newGridSquare = document.createElement("div");
+        newGridSquare.setAttribute("id", i);
+        newGridSquare.setAttribute("class", "square");
+        newGridSquare.innerText = `${i}`;
+        document.body.appendChild(newGridSquare);
+        if(i%4==0){
+            createLineBreakDiv();
+        }
+    }
+}
+function createLineBreakDiv(){
+    const lineBreakDiv = document.createElement("div");
+    lineBreakDiv.setAttribute("class", "break");
+    document.body.appendChild(lineBreakDiv);
+}
+
+createGridSquares();
